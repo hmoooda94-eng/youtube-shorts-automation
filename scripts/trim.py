889b4f2d6bf98799b3f447aefbd3b9c4a98bed92
hmoooda94-eng <hmoooda94@gmail.com>
2026-01-18 22:59:14 +0100

    Update trim.py

--- scripts/trim.py
+++ scripts/trim.py
@@ -1,1 +1,30 @@
 
+import subprocess
+import os
+
+INPUT_VIDEO = "videos/input.mp4"
+OUTPUT_VIDEO = "videos/trimmed.mp4"
+
+START_TIME = "00:00:05"   # يبدأ من الثانية 5
+DURATION = "30"           # مدة 30 ثانية
+
+def trim_video():
+    if not os.path.exists(INPUT_VIDEO):
+        print("❌ input.mp4 غير موجود")
+        return
+
+    command = [
+        "ffmpeg",
+        "-y",
+        "-i", INPUT_VIDEO,
+        "-ss", START_TIME,
+        "-t", DURATION,
+        "-c", "copy",
+        OUTPUT_VIDEO
+    ]
+
+    subprocess.run(command)
+    print("✅ تم قص الفيديو بنجاح")
+
+if __name__ == "__main__":
+    trim_video()
